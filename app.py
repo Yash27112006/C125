9dc39604e8f6c304eaaa9dc63b11562de35c7421
Yash27112006 <64268919+Yash27112006@users.noreply.github.com>
2021-07-25 09:12:35 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify, request
+from classifier import get_prediction
+
+app = Flask(__name__)
+@app.route('/predict-digit',methods=["POST"])
+
+def predict_data():
+    image = request.files.get("digit")
+    predicition = get_prediction(image)
+    return jsonify(
+        {
+            "prediction": predicition
+        }
+    ),
+    200
+
+if __name__ == "__main__":
+    app.run(debug=True)
